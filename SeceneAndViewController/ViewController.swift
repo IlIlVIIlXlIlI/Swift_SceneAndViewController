de292ae1d8964ad2b3d09740cb509debbef43bc4
Shogo <vanitas710@yahoo.co.jp>
2021-01-09 05:38:51 +0900

    HomeからYosemite画面へ遷移 Yosemite画面からHomeへ戻るようにした

--- SeceneAndViewController/ViewController.swift
+++ SeceneAndViewController/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func comeHome(segue: UIStoryboardSegue){
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
